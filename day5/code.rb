b94b36a5a51e9e59520e252800fd6fe613f7f02e
Mark Alexiuk <mark.alexiuk@vetaffi.com>
2024-12-07 11:38:03 -0600

    fix for value type

--- day5/code.rb
+++ day5/code.rb
@@ -20,7 +20,7 @@ def get_manuals(fname)
   manuals = []
 
   File.foreach(fname) do |line|
-    manuals << line.chomp.split(',')
+    manuals << line.chomp.split(',').map(&:to_i)
 
   end
   manuals 
@@ -28,8 +28,10 @@ end
 
 manuals = get_manuals(manuals_fname)
 
-new_manuals = manuals[0...1]
+new_manuals = manuals[3...4]
 manuals = new_manuals
+
+puts "num manuals = #{manuals.length}"
 
 manuals.each do |manual|
   puts manual
@@ -37,12 +39,34 @@ manuals.each do |manual|
 end
 
 def is_before?(list, val1, val2)
+
+  puts "\n\ncheck"
+  puts "val1 = #{val1}"
+  puts "val2 = #{val2}"
+  puts list 
+
   idx1 = list.index(val1)
   idx2 = list.index(val2)
 
-  return false if idx1.nil? || idx2.nil? || idx1 > idx2 
+  # if either index is not present, the rule is
+  # satisfied.
+  return true if idx1.nil? || idx2.nil?
+
+  return false if idx1 > idx2 
   true 
 end 
+
+
+# test
+# puts "check is before"
+# a = [1,2,3]
+# puts a 
+# # puts is_before?(a, 2, 3)
+# # puts is_before?(a, 3, 1)
+# # puts is_before?(a, 7, 1)
+# puts is_before?(a, 2, 5)
+# puts "----"
+
 
 def manual_is_ordered_correct?(manual, rules)
   
@@ -53,7 +77,11 @@ def manual_is_ordered_correct?(manual, rules)
   manual.each do |page|
 
     rules.each do |rule|
-      if rule[0] == page 
+
+      if rule[0] == page
+        # puts 'hit'
+        # puts "rule: #{rule}"
+        # puts "page: #{page}\n"
         ordered_correct = is_before?(manual, page, rule[1])
         if !ordered_correct
           return false
@@ -64,6 +92,15 @@ def manual_is_ordered_correct?(manual, rules)
 
   ordered_correct
 end
+
+
+# manual_a = [1,2,3]
+# rules_a = []
+# rules_a << [1,2]
+# rules_a << [2,3]
+# rules_a << [3,1]
+# puts manual_is_ordered_correct?(manual_a, rules_a)
+# puts "========="
 
 
 correct_manuals = []
